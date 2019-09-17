db38e62551a8a75b9873c3f725bec5f5bafd1794
simssd <51689363+simssd@users.noreply.github.com>
2019-09-17 11:08:11 +0700

    Update HelloController.php

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -1,3 +1,13 @@
 <?php
 
-names
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    function showHello(){
+       return '<h1>Hollo controller</h1>'
+    }
+}
+         
