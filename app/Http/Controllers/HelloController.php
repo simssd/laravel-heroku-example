d61e17039f5ca6a04b69ec5e138cfecb4772ccd9
simssd <51689363+simssd@users.noreply.github.com>
2019-10-01 09:56:05 +0700

    Update HelloController.php

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    function showHello($name){
-       return '<h1>Hello Controller</h1>';
+    function showHello(){
+       return '<h1>Hello Controller : Srisuda Honghwean</h1>';
     }
 }
