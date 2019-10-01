0f076f2867574a6cabd3a0e0b3c9f138a6ae01e0
simssd <51689363+simssd@users.noreply.github.com>
2019-10-01 10:23:46 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('users',function() {
-    return  '<h1>Users!  555</h1>';
-});
-Route::get('showname', function() {
-    return  '<h1>Hello Laravel</h1>';
-});
-Route::get('welcome/name','Hellocontroller@showHello');    
+Route::get('showname/{name}','Hellocontroller@showHello');
