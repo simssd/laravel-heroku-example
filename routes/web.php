7875e4543fa56d084e456a02175dad8d7913ed57
simssd <51689363+simssd@users.noreply.github.com>
2019-10-01 12:52:02 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/homes', function () {
-    return view('homes');
+Route::get('users', function () {
+    return '<h1>Users! 555</h1>';
 });
-Route::get('/edit', function () {
-    return view('edit');
+Route::get('showname', function () {
+    return '<h1>Hello Laraval</h1>';
 });
-Route::get('/show','HelloController@show');
+
 
